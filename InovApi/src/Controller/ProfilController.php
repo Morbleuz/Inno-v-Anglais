1777f2423c9b08d3a9feb819ee07ef33acca0a87
Axel <axel2003@outlook.com>
2022-11-15 09:46:28 +0100

    page profil ok

--- InovApi/src/Controller/ProfilController.php
+++ InovApi/src/Controller/ProfilController.php
@@ -4,15 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Form\UserType;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager,Request $request): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'form'=>$form->createView()
         ]);
     }
 }
